6a3f1b9bba81fb834ee5b7963b5df13a55160c62
Niloo Mont <niloo.mon@gmail.com>
2023-12-23 15:57:42 +0330

    Implemented Content Coupling

---
+++ sources/Niloo Mont/Coupling/ContentCoupling/Program.cs
@@ -0,0 +1,10 @@
+﻿using ContentCoupling;
+
+UserManagement userManagement = new() { 
+    UserName = "User1",
+    Password = "123"
+};
+userManagement.RegisterUser();
+
+AccountManagement accountManagement = new();
+accountManagement.ResetUserPassword(userManagement, "abc");
---
+++ sources/Niloo Mont/Coupling/ContentCoupling/UserManagement.cs
@@ -0,0 +1,10 @@
+﻿namespace ContentCoupling;
+public class UserManagement
+{
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+    public void RegisterUser()
+    {
+        Console.WriteLine($"User: {UserName} is registered. Password is set to: {Password}");
+    }
+}

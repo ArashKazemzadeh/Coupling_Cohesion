45fb73b0f3a17cb1e0bb3c4cbfdb753796f85723
Niloo Mont <niloo.mon@gmail.com>
2023-12-23 16:56:45 +0330

    Implemented Functional Cohesion

---
+++ sources/Niloo Mont/Cohesion/FunctionalCohesion/ProductManagement.cs
@@ -0,0 +1,31 @@
+﻿using Dumpify;
+
+namespace FunctionalCohesion;
+public class Product
+{
+    public required int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class ProductManagement
+{
+    public void AddProduct(List<Product> products,Product product)
+    {
+        products.Add(product);
+        $"product {product.Name} was added.".Dump();
+    }
+    public void GetProductById(List<Product> products,int id)
+    {
+        products.SingleOrDefault(p => p.Id == id).Dump();
+    }
+    public void GetAllProducts(List<Product> products)
+    {
+        products.Dump();
+    }
+    public void DeleteProductById(List<Product> products,int id)
+    {
+        var product = products.SingleOrDefault(p => p.Id == id);
+        products.Remove(product);
+        $"product {product.Name} was deleted.".Dump();
+    }
+}

804b059e33823ae7a5e6778b56fd2a0b0ed57264
Niloo Mont <niloo.mon@gmail.com>
2023-12-23 14:56:29 +0330

    Implemented Control Coupling

---
+++ sources/Niloo Mont/Coupling/Control Coupling/Product.cs
@@ -0,0 +1,7 @@
+﻿namespace Control_Coupling;
+public class Product
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public ushort Quantity { get; set; } = 0;
+}
